8b65b46a7de0fe7a2372f2e17d1d6b3f23f0531b
Donald Sincennes <donaldsincennes@hotmail.com>
2022-09-02 02:01:50 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import logo from './logo.svg';
+import logo from './amano.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h2><b>React and Dockers</b></h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi there!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://tracker.gg/valorant/profile/riot/XmanJ%2312345/overview"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Best player in valorant!!
+        </a>
+        <p></p>
+        <a
+          className="App-link"
+          href="https://tracker.gg/valorant/profile/riot/Awais%23613/overview"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Worst player in valorant
         </a>
       </header>
     </div>
